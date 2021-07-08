c0be062a1de3f38cb9ca7143eadf156a22e47404
Dmyryi <65827238+Dmyryi@users.noreply.github.com>
2021-07-08 17:37:27 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,10 @@
-function onInput(event) {
-    textEl.style.fontSize = event.currentTarget.value + "px";
-  }
+const inputRangeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+const onInputRateChange = (event) =>{
+  textEl.style.fontSize = event.target.value * 0.32 + 'px';
+};
+
+inputRangeEl.addEventListener('input', onInputRateChange);
+
+const fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0]);
